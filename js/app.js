774daaa5a7d33aa290670a6c9080c88768faf036
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-09-20 13:45:18 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (() => {
     "use strict";
+    const modules_flsModules = {};
     function isWebp() {
         function testWebP(callback) {
             let webP = new Image;
@@ -59,6 +60,66 @@
             }
         }));
     }
+    function functions_FLS(message) {
+        setTimeout((() => {
+            if (window.FLS) console.log(message);
+        }), 0);
+    }
+    class MousePRLX {
+        constructor(props, data = null) {
+            let defaultConfig = {
+                init: true,
+                logging: true
+            };
+            this.config = Object.assign(defaultConfig, props);
+            if (this.config.init) {
+                const paralaxMouse = document.querySelectorAll("[data-prlx-mouse]");
+                if (paralaxMouse.length) {
+                    this.paralaxMouseInit(paralaxMouse);
+                    this.setLogging(`Проснулся, слежу за объектами: (${paralaxMouse.length})`);
+                } else this.setLogging("Нет ни одного объекта. Сплю...zzZZZzZZz...");
+            }
+        }
+        paralaxMouseInit(paralaxMouse) {
+            paralaxMouse.forEach((el => {
+                const paralaxMouseWrapper = el.closest("[data-prlx-mouse-wrapper]");
+                const paramСoefficientX = el.dataset.prlxCx ? +el.dataset.prlxCx : 100;
+                const paramСoefficientY = el.dataset.prlxCy ? +el.dataset.prlxCy : 100;
+                const directionX = el.hasAttribute("data-prlx-dxr") ? -1 : 1;
+                const directionY = el.hasAttribute("data-prlx-dyr") ? -1 : 1;
+                const paramAnimation = el.dataset.prlxA ? +el.dataset.prlxA : 50;
+                let positionX = 0, positionY = 0;
+                let coordXprocent = 0, coordYprocent = 0;
+                setMouseParallaxStyle();
+                if (paralaxMouseWrapper) mouseMoveParalax(paralaxMouseWrapper); else mouseMoveParalax();
+                function setMouseParallaxStyle() {
+                    const distX = coordXprocent - positionX;
+                    const distY = coordYprocent - positionY;
+                    positionX += distX * paramAnimation / 1e3;
+                    positionY += distY * paramAnimation / 1e3;
+                    el.style.cssText = `transform: translate3D(${directionX * positionX / (paramСoefficientX / 10)}%,${directionY * positionY / (paramСoefficientY / 10)}%,0);`;
+                    requestAnimationFrame(setMouseParallaxStyle);
+                }
+                function mouseMoveParalax(wrapper = window) {
+                    wrapper.addEventListener("mousemove", (function(e) {
+                        const offsetTop = el.getBoundingClientRect().top + window.scrollY;
+                        if (offsetTop >= window.scrollY || offsetTop + el.offsetHeight >= window.scrollY) {
+                            const parallaxWidth = window.innerWidth;
+                            const parallaxHeight = window.innerHeight;
+                            const coordX = e.clientX - parallaxWidth / 2;
+                            const coordY = e.clientY - parallaxHeight / 2;
+                            coordXprocent = coordX / parallaxWidth * 100;
+                            coordYprocent = coordY / parallaxHeight * 100;
+                        }
+                    }));
+                }
+            }));
+        }
+        setLogging(message) {
+            this.config.logging ? functions_FLS(`[PRLX Mouse]: ${message}`) : null;
+        }
+    }
+    modules_flsModules.mousePrlx = new MousePRLX({});
     function ssr_window_esm_isObject(obj) {
         return null !== obj && "object" === typeof obj && "constructor" in obj && obj.constructor === Object;
     }
@@ -3512,6 +3573,76 @@
     window.addEventListener("load", (function(e) {
         initSliders();
     }));
+    class parallax_Parallax {
+        constructor(elements) {
+            if (elements.length) this.elements = Array.from(elements).map((el => new parallax_Parallax.Each(el, this.options)));
+        }
+        destroyEvents() {
+            this.elements.forEach((el => {
+                el.destroyEvents();
+            }));
+        }
+        setEvents() {
+            this.elements.forEach((el => {
+                el.setEvents();
+            }));
+        }
+    }
+    parallax_Parallax.Each = class {
+        constructor(parent) {
+            this.parent = parent;
+            this.elements = this.parent.querySelectorAll("[data-prlx]");
+            this.animation = this.animationFrame.bind(this);
+            this.offset = 0;
+            this.value = 0;
+            this.smooth = parent.dataset.smooth ? Number(parent.dataset.smooth) : 15;
+            this.setEvents();
+        }
+        setEvents() {
+            this.animationID = window.requestAnimationFrame(this.animation);
+        }
+        destroyEvents() {
+            window.cancelAnimationFrame(this.animationID);
+        }
+        animationFrame() {
+            const topToWindow = this.parent.getBoundingClientRect().top;
+            const heightParent = this.parent.offsetHeight;
+            const heightWindow = window.innerHeight;
+            const positionParent = {
+                top: topToWindow - heightWindow,
+                bottom: topToWindow + heightParent
+            };
+            const centerPoint = this.parent.dataset.center ? this.parent.dataset.center : "center";
+            if (positionParent.top < 30 && positionParent.bottom > -30) switch (centerPoint) {
+              case "top":
+                this.offset = -1 * topToWindow;
+                break;
+
+              case "center":
+                this.offset = heightWindow / 2 - (topToWindow + heightParent / 2);
+                break;
+
+              case "bottom":
+                this.offset = heightWindow - (topToWindow + heightParent);
+                break;
+            }
+            this.value += (this.offset - this.value) / this.smooth;
+            this.animationID = window.requestAnimationFrame(this.animation);
+            this.elements.forEach((el => {
+                const parameters = {
+                    axis: el.dataset.axis ? el.dataset.axis : "v",
+                    direction: el.dataset.direction ? el.dataset.direction + "1" : "-1",
+                    coefficient: el.dataset.coefficient ? Number(el.dataset.coefficient) : 5,
+                    additionalProperties: el.dataset.properties ? el.dataset.properties : ""
+                };
+                this.parameters(el, parameters);
+            }));
+        }
+        parameters(el, parameters) {
+            if ("v" == parameters.axis) el.style.transform = `translate3D(0, ${(parameters.direction * (this.value / parameters.coefficient)).toFixed(2)}px,0) ${parameters.additionalProperties}`; else if ("h" == parameters.axis) el.style.transform = `translate3D(${(parameters.direction * (this.value / parameters.coefficient)).toFixed(2)}px,0,0) ${parameters.additionalProperties}`;
+        }
+    };
+    if (document.querySelectorAll("[data-prlx-parent]")) modules_flsModules.parallax = new parallax_Parallax(document.querySelectorAll("[data-prlx-parent]"));
     let addWindowScrollEvent = false;
     function headerScroll() {
         addWindowScrollEvent = true;
